c8e251540a0bcbe7376074c6ef77511f5053a33b
Jeferson Schlarski <jefersonunik@gmail.com>
2023-08-11 03:45:23 -0300

    table

---
+++ resources/views/pages/produtos/paginacao.blade.php
@@ -0,0 +1,39 @@
+{{-- extends da index --}}
+@extends('index')
+
+@section('content')
+    <div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pb-2 mb-3 border-bottom">
+    <h1 >Produtos</h1>
+    </div>
+    <div>
+        <form action="" method="get">
+            <input type="text" name="pesquisar" placeholder="Digite um nome">
+            <button>Pesquisar</button>
+            <a type="button" class="btn btn-success float-right">Incluir produto</a>
+        </form>
+      <div class="mt-4 table-responsive small">
+        <table class="table table-striped table-sm">
+          <thead>
+            <tr>
+              <th scope="col">Nome</th>
+              <th scope="col">Valor</th>
+              <th scope="col">Ações</th>
+            </tr>
+          </thead>
+          <tbody>
+            @foreach ($findProduto as $produto)
+            <tr>
+              <td class="align-middle">{{$produto->nome}}</td>
+              <td class="align-middle">{{'R$' . number_format($produto->valor, 2, ',', '.' )}}</td>
+              <td>
+                <a href="" class="btn btn-light btn-sm">Editar</a>
+                <a href="" class="btn btn-danger btn-sm">Excluir</a>
+              </td>
+              </tr>  
+            @endforeach
+           
+          </tbody>
+        </table>
+      </div>
+    </div>
+@endsection
